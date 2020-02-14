646cb7003148cdfce1baff6cf1648a90c1343ed0
Gustave P <git@gusp.io>
2020-02-14 17:01:03 +0100

    Deleted closure to cache routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/debug-sentry', function () {
-    throw new Exception('My first Sentry error!');
-});
+Route::get('/', 'WelcomeController@welcome');
